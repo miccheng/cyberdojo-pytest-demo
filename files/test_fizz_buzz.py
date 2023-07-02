1708b72f97704271650d10837a36b253f8beca9a
H9qLk4 <H9qLk4@cyber-dojo.org>
2023-07-02 05:43:53 +0000

    29 ran tests, no prediction, got amber

--- files/test_fizz_buzz.py
+++ files/test_fizz_buzz.py
@@ -1,5 +1,21 @@
 from fizz_buzz import fizz_buzz
 
+
+@pytest.mark.parametrize(
+    "test_input,expected", 
+    [
+        (0, []),
+        (1, [1]), 
+        (3, [1, 2, 'Fizz']),
+        (5, [1, 2, 'Fizz', 4, 'Buzz']),
+        (15, [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz'])
+    ]
+)
+def test_fizz_buzz(test_input, expected):
+    assert fizz_buzz(test_input) == expected
+    
+
+'''
 def test_empty_list():
     assert fizz_buzz(0) == []
 
@@ -14,3 +30,4 @@ def test_first_multiple_of_5():
     
 def test_first_multiple_of_3_and_5():
     assert fizz_buzz(15) == [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
+'''
