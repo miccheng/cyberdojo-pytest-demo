a4ed5af16cc3cc4f540fa83fc96bc8731ca782b5
H9qLk4 <H9qLk4@cyber-dojo.org>
2023-07-02 05:57:33 +0000

    41 ran tests, no prediction, got green

--- files/test_fizz_buzz.py
+++ files/test_fizz_buzz.py
@@ -14,21 +14,3 @@ from fizz_buzz import fizz_buzz
 def test_fizz_buzz(num, expected):
     print('Expecting input of {} will result in {}'.format(num, expected))
     assert fizz_buzz(num) == expected
-    
-
-'''
-def test_empty_list():
-    assert fizz_buzz(0) == []
-
-def test_one_item():
-    assert fizz_buzz(1) == [1]
-
-def test_first_multiple_of_3():
-    assert fizz_buzz(3) == [1, 2, 'Fizz']
-    
-def test_first_multiple_of_5():
-    assert fizz_buzz(5) == [1, 2, 'Fizz', 4, 'Buzz']
-    
-def test_first_multiple_of_3_and_5():
-    assert fizz_buzz(15) == [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
-'''
