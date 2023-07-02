187e78b0ca02706ada16533ca94d85967c4cb0ee
H9qLk4 <H9qLk4@cyber-dojo.org>
2023-07-02 05:57:50 +0000

    42 ran tests, no prediction, got green

--- files/test_fizz_buzz.py
+++ files/test_fizz_buzz.py
@@ -8,7 +8,8 @@ from fizz_buzz import fizz_buzz
         (1, [1]),
         (3, [1, 2, 'Fizz']),
         (5, [1, 2, 'Fizz', 4, 'Buzz']),
-        (15, [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz'])
+        (15, [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz',
+              'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz'])
     ]
 )
 def test_fizz_buzz(num, expected):
