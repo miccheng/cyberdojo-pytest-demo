f1681edaea48f0cc4c5ad9b647fc385601fc015c
H9qLk4 <H9qLk4@cyber-dojo.org>
2023-07-02 05:30:49 +0000

    26 ran tests, no prediction, got green

--- files/test_fizz_buzz.py
+++ files/test_fizz_buzz.py
@@ -13,4 +13,4 @@ def test_first_multiple_of_5():
     assert fizz_buzz(5) == [1, 2, 'Fizz', 4, 'Buzz']
     
 def test_first_multiple_of_3_and_5():
-    assert fizz_buzz(5) == [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
+    assert fizz_buzz(15) == [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
